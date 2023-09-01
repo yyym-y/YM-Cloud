13fb31cbf9aa700ea03d8cef0c003f08201a1add
yyym <3109778990@qq.com>
2023-09-01 18:17:42 +0800

    后端代码上传

---
+++ cloud-back/src/main/java/org/yyym/cloudback/CloudBackApplication.java
@@ -0,0 +1,13 @@
+package org.yyym.cloudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBackApplication.class, args);
+	}
+
+}
---
+++ cloud-back/src/test/java/org/yyym/cloudback/CloudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yyym.cloudback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
